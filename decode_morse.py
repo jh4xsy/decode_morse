d733ef0f310f6914eecfc1b955eac1a6508fca28
Hiroshi Iwamoto <sx7h-iwmt@asahi-net.or.jp>
2024-04-04 10:22:37 +0900

    Pass the WAV filename as an argument

--- decode_morse.py
+++ decode_morse.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import argparse
+
 import numpy as np
 import scipy.io.wavfile
 from scipy import signal
@@ -7,33 +9,38 @@ from scipy import signal
 from main import num_tags, prediction_to_str
 from morse import ALPHABET, SAMPLE_FREQ, get_spectrogram
 
-rate, data = scipy.io.wavfile.read("9284942.wav")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input")
+    args = parser.parse_args()
 
-# Resample and rescale
-length = len(data) / rate
-new_length = int(length * SAMPLE_FREQ)
+    rate, data = scipy.io.wavfile.read(args.input)
 
-data = signal.resample(data, new_length)
-data = data.astype(np.float32)
-data /= np.max(np.abs(data))
+    # Resample and rescale
+    length = len(data) / rate
+    new_length = int(length * SAMPLE_FREQ)
 
-# Create spectrogram
-spec = get_spectrogram(data)
-spec_orig = spec.copy()
-spectrogram_size = spec.shape[0]
+    data = signal.resample(data, new_length)
+    data = data.astype(np.float32)
+    data /= np.max(np.abs(data))
 
-spec = spec.transpose(1,0)
-spec = np.expand_dims(spec, axis=0)
+    # Create spectrogram
+    spec = get_spectrogram(data)
+    spec_orig = spec.copy()
+    spectrogram_size = spec.shape[0]
 
-import onnxruntime as ort
+    spec = spec.transpose(1,0)
+    spec = np.expand_dims(spec, axis=0)
 
-sess = ort.InferenceSession('nn-morse.onnx')
-output = sess.run(['output'], {'input': spec})
+    import onnxruntime as ort
 
-output1 = np.array(output[0][0])
-y_pred_l = np.exp(output1.tolist())
+    sess = ort.InferenceSession('nn-morse.onnx')
+    output = sess.run(['output'], {'input': spec})
 
-# Convert prediction into string
-# TODO: proper beam search
-m = np.argmax(output1, 1)
-print(prediction_to_str(m))
+    output1 = np.array(output[0][0])
+    y_pred_l = np.exp(output1.tolist())
+
+    # Convert prediction into string
+    # TODO: proper beam search
+    m = np.argmax(output1, 1)
+    print(prediction_to_str(m))
